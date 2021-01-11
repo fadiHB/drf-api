8f9a1b8de5b9159205064f363cb46040bb98285b
fadiHB <eng.fadi.hb@gmail.com>
2021-01-11 22:11:27 +0200

    lab32

--- jokes/views.py
+++ jokes/views.py
@@ -4,12 +4,16 @@ from rest_framework.generics import ListAPIView, RetrieveAPIView, RetrieveDestro
 from .serializer import JokesSerializer
 from .models import Jokes
 
+from .permissions import PermissionsClass
+
 # Create your views here.
 class JokesListView(ListAPIView):
     queryset = Jokes.objects.all()
     serializer_class = JokesSerializer
+    permissions_classes = (PermissionsClass,)
 
 class JokesDetailsView(RetrieveUpdateDestroyAPIView):
     queryset = Jokes.objects.all()
     serializer_class = JokesSerializer
+    permissions_classes = (PermissionsClass,)
     
